73caee4662667811fd0521420be4bf49f0fce38e
jug-ed <rachagovartyman@gmail.com>
2025-06-17 22:31:55 +0300

    Update multilanguage

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const popupList = ['Простота — высшая степень утончённости', 'Убирай лишнее, оставляй важное', 'Минимализм — путь к гармонии', 'В простоте – красота', 'Минимализм — это сохранение ценного и отказ от отвлекающего', 'Простота — стиль жизни', 'Меньше хлама — больше свободы'];
-
-let curPopInx = 0;
-
-function popupTimeShow() {
-	$('#popup_text').text(popupList[curPopInx]);
-	$('#popup').css('opacity', '100%');
-	setTimeout(() => {
-		$('#popup').css('opacity', '0%');
-	}, 3000);
-
-	curPopInx = (curPopInx + 1) % popupList.length;
-	setTimeout(popupTimeShow, 15000);
-}
-
+var usrLng = navigator.languages[0];
 
 $(document).ready(function() {
 	$('.head_burg,.head_link').click(function(event) {
@@ -21,6 +7,40 @@ $(document).ready(function() {
 
 	setTimeout(popupTimeShow, 10000);
 });
+
+
+
+function lngTxtRange(lang) {
+	for (let key in lngArr) {
+		let el = document.getElementById("lng-"+key);
+		if (el) {
+			el.innerHTML = lngArr[key][lang];
+		}
+	}
+}
+lngTxtRange(usrLng);
+
+
+
+const popupList = {
+	"en-US":['Simplicity is the highest degree of sophistication', 'Remove the unnecessary, leave the important', 'Minimalism is the path to harmony', 'There is beauty in simplicity', 'Minimalism is preserving what is valuable and rejecting what is distracting', 'Simplicity is a lifestyle', 'Less clutter - more freedom'],
+	"ru-RU":['Простота — высшая степень утончённости', 'Убирай лишнее, оставляй важное', 'Минимализм — путь к гармонии', 'В простоте – красота', 'Минимализм — это сохранение ценного и отказ от отвлекающего', 'Простота — стиль жизни', 'Меньше хлама — больше свободы']
+}
+
+let curPopInx = 0;
+
+function popupTimeShow() {
+	try { $('#popup_text').text(popupList[usrLng][curPopInx]); }
+	catch { $('#popup_text').text(popupList["en-US"][curPopInx]); }
+	$('#popup').css('opacity', '100%');
+	setTimeout(() => {
+		$('#popup').css('opacity', '0%');
+	}, 3000);
+
+	try { curPopInx = (curPopInx + 1) % popupList[usrLng].length; }
+	catch { curPopInx = (curPopInx + 1) % popupList["en-US"].length; }
+	setTimeout(popupTimeShow, 15000);
+}
 
 
 
@@ -35,6 +55,6 @@ const observer = new IntersectionObserver((entries) => {
 	threshold: 0.1
 });
 
-document.querySelectorAll('.blk, .about p, #about, #project').forEach(el => {
+document.querySelectorAll('.blk, .about p, #lng-about, #lng-project').forEach(el => {
 	observer.observe(el);
 });
---
+++ strings.js
@@ -0,0 +1,114 @@
+const lngArr = {
+    "btn-service":{
+        "en-US":"Service",
+        "ru-RU":"Услуги"
+    },
+    "btn-project":{
+        "en-US":"Projects",
+        "ru-RU":"Проекты"
+    },
+    "btn-about":{
+        "en-US":"About",
+        "ru-RU":"Обо мне"
+    },
+    "main-title":{
+        "en-US":"Author of your ideas",
+        "ru-RU":"Автор твоих идей"
+    },
+    "main-text":{
+        "en-US":"Development of web-sites, applications and scripts for various tasks",
+        "ru-RU":"Разработка веб сайтов, приложений и скриптов под разные задачи"
+    },
+    "service":{
+        "en-US":"Service",
+        "ru-RU":"Услуги"
+    },
+    "app":{
+        "en-US":"Application",
+        "ru-RU":"Приложение"
+    },
+    "app-text":{
+        "en-US":"Creating applications for Windows and Android. Managers, trackers, educational applications, applications for interaction with API, interactive applications.",
+        "ru-RU":"Создание приложений под Windows и Android. Менеджеры, трекеры, обучающие приложения, приложения для взаимодействия с API, интерактивные приложения."
+    },
+    "app-cost":{
+        "en-US":"From 5 000 rub.",
+        "ru-RU":"От 5 000 руб."
+    },
+    "web":{
+        "en-US":"Web-site",
+        "ru-RU":"Сайт"
+    },
+    "web-text":{
+        "en-US":"Development of websites and web applications. Websites with CMS, blogs, online stores (without online payment), information, business cards.",
+        "ru-RU":"Разработка сайтов и веб-приложений. Сайты с CMS, блоги, интернет-магазины (без онлайн оплаты), информационные, визитки."
+    },
+    "web-cost":{
+        "en-US":"From 1 000 rub.",
+        "ru-RU":"От 1 000 руб."
+    },
+    "script":{
+        "en-US":"Script",
+        "ru-RU":"Скрипт"
+    },
+    "script-text":{
+        "en-US":"Scripts for solving and automating various tasks, as well as writing code for educational assignments.",
+        "ru-RU":"Скрипты для решения и автоматизации различных задач, а так же написание кода по учебным заданиям."
+    },
+    "script-cost":{
+        "en-US":"From 500 rub.",
+        "ru-RU":"От 500 руб."
+    },
+    "project":{
+        "en-US":"Projects",
+        "ru-RU":"Проекты"
+    },
+    "about":{
+        "en-US":"About",
+        "ru-RU":"Обо мне"
+    },
+    "about-text1":{
+        "en-US":"Hi there, JugEd with you",
+        "ru-RU":"Привет, с вами JugEd"
+    },
+    "about-text2":{
+        "en-US":"I've been working in IT for over 2 years, and I started my journey 7 years ago.",
+        "ru-RU":"Работаю более 2 лет в IT, а начал свой путь еще 7 лет назад"
+    },
+    "about-text3":{
+        "en-US":"I appreciate minimalism and convenience",
+        "ru-RU":"Ценю минимализм и удобство"
+    },
+    "about-text4":{
+        "en-US":"I'll create a convenient application or website for your needs",
+        "ru-RU":"Создам удобное приложение или сайт под твои задачи"
+    },
+    "about-text5":{
+        "en-US":"And if you need help with the code for homework, I'll help fast and chip",
+        "ru-RU":"А если нужна помощь с кодом для ДЗ, то помогу быстро и недорого"
+    },
+    "project1":{
+        "en-US":"Vash zamok",
+        "ru-RU":"Ваш замок"
+    },
+    "project1-text":{
+        "en-US":"Lock shop website. A wide variety of locks, keys, and keychains. There are repair services, lock replacement, and creation of duplicate keys. A pleasant bonus is the service of creating business cards and pendants using laser engraving.",
+        "ru-RU":"Сайт магазина замков. Большое разнообразие замков, ключей, а так же брелоков. Есть услуги ремонта, замены замков и создание дубликатов ключей. Приятным бонусом служит услуга по созданию визиток и кулонов при помощи лазерной гравировки."
+    },
+    "project1-btn":{
+        "en-US":"Go to website",
+        "ru-RU":"Перейти на сайт"
+    },
+    "project2":{
+        "en-US":"Space model",
+        "ru-RU":"Space model"
+    },
+    "project2-text":{
+        "en-US":"Simulation of the behavior of bodies in space. Developed in python using pygame and pymunk, and tkinter was used for the interface. Realistic visualization of weightlessness, gravity and the impact of space bodies on each other.",
+        "ru-RU":"Симуляция поведения тел в космосе. Разработана на python с использованием pygame и pymunk, а для интерфейса был использован tkinter. Реалистичная визуализация невесомости, силы притяжения и воздействия космических тел друг на друга."
+    },
+    "project2-btn":{
+        "en-US":"Go to project",
+        "ru-RU":"Перейти на проект"
+    },
+}
